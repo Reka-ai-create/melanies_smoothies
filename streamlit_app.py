b4abb94cb4fb18f9c286e9173146b89b6154d609
Reka-ai-create <rekareklam4@gmail.com>
2025-09-05 14:38:00 +0200

    Update streamlit_app.py
    When you bring in pandas to the Py file, call it pd in the same way we shorten streamlit to st. This is common in the Python coding world and it makes pandas easier to refer to.

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,15 @@ st.write('The name on your Smoothie:', name_on_order)
 # Load fruit options
 cnx = st.connection("snowflake")
 session = cnx.session()
+
 my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop
+
+#Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 # Multiselect ingredients
 ingredients_list = st.multiselect(
