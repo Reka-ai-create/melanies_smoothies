ef7db6352d7d703b34dc062e70e65ceb06cceac8
Reka-ai-create <rekareklam4@gmail.com>
2025-09-05 14:59:58 +0200

    Update streamlit_app.py
    Making Use of the SEARCH_ON Value

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,22 +35,21 @@ ingredients_list = st.multiselect(
     , max_selections=5
 )
 
-if ingredients_list and name_on_order:
-    # Build the string with a loop
+if ingredients_list:
     ingredients_string = ""
   
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
         st.subheader(fruit_chosen + 'Nutrition Infromation')  
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     # Trim trailing space (optional)
-    ingredients_string = ingredients_string.strip()
+    #st.write(ingredients_string)
 
     # Create insert statement
     my_insert_stmt = f"""
