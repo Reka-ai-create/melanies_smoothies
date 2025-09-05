6387803dcbd8ef50bb1e9953f4167a6f2b86a266
Reka-ai-create <rekareklam4@gmail.com>
2025-09-05 14:01:14 +0200

    Update streamlit_app.py
    Let's Get the SmoothieFroot Data to Show Nutrition Data for the Fruits Chosen
    To do this, we need to rearrange our code lines.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title("Customize Your Smoothie :cup_with_straw:")
@@ -31,6 +32,8 @@ if ingredients_list and name_on_order:
     ingredients_string = ""
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # Trim trailing space (optional)
     ingredients_string = ingredients_string.strip()
@@ -45,9 +48,3 @@ if ingredients_list and name_on_order:
     if st.button('Submit Order'):
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-
-# New section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
