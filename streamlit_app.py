d77c52c1983d30ec2ddada40a2c29cc4478bf5c9
Reka-ai-create <rekareklam4@gmail.com>
2025-09-05 14:07:46 +0200

    Update streamlit_app.py
     Use Our fruit_chosen Variable in the API Call

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,9 +30,11 @@ ingredients_list = st.multiselect(
 if ingredients_list and name_on_order:
     # Build the string with a loop
     ingredients_string = ""
+  
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + 'Nutrition Infromation')  
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # Trim trailing space (optional)
